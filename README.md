7fb0922e4ea530fe0c23282c42f39582c2bd3bf6
masserg <masserg2007@gmail.com>
2020-01-08 10:34:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# INSTALL
+
+create database & edit .env
+
+```bash
+composer create-project --prefer-dist laravel/laravel chat "5.7.*"
+php artisan make:auth
+php artisan migrate
+```
+
+Redis
+
+```bash
+sudo apt install redis-server
+composer require predis/predis
+```
+
+Edit .env
+
+```bash
+BROADCAST_DRIVER=redis
+CACHE_DRIVER=file
+QUEUE_CONNECTION=sync
+SESSION_DRIVER=file
+SESSION_LIFETIME=120
+
+REDIS_HOST=127.0.0.1
+REDIS_PASSWORD=null
+REDIS_PORT=6379
+```
+Edit /config/app.php
+
+```bash
+App\Providers\BroadcastServiceProvider::class,
+```
+
+Clear config cache
+
+```bash
+php artisan config:cache
+```
+
+Laravel-echo-server
+
+```bash
+sudo npm install -g laravel-echo-server
+laravel-echo-server init
+laravel-echo-server start
+```
+
+Libs & watch
+
+```bash
+npm install
+npm install --save laravel-echo
+npm install --save socket.io-client
+npm run watch-poll
+```
+
+Queue
+
+```bash
+php artisan queue:work
+```
+
+Start server
+
+```bash
+php artisan serve
+```
+
+Open url in two browsers: [http://localhost:8000](http://localhost:8000)
---
+++ resources/js/components/PrivateChat.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="container">
+        <hr>
+        <div class="row">
+            <div class="col-sm-12">
+                <textarea class="form-control" rows="10" readonly="">{{messages.join('\n')}}</textarea>
+                <hr>
+                <input type="text" class="form-control" v-model="textMessage" @keyup.enter="sendMessage" @keydown="actionUser">
+                <span v-if="isActive">{{isActive.name}} is typing...</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['room', 'user'],
+        data() {
+            return {
+                messages: [],
+                textMessage: '',
+                isActive: false,
+                typingTimer: false
+            }
+        },
+        computed: {
+            channel(){
+                return window.Echo.private('room.' + this.room.id);
+            }
+        },
+        mounted() {
+            this.channel
+                .listen('PrivateChat', ({data}) => {
+                    this.messages.push(data.body);
+                    this.isActive = false;
+                })
+                .listenForWhisper('typing', (e) =>{
+                    this.isActive = e;
+                    if(this.typingTimer) clearTimeout(this.typingTimer);
+                    this.typingTimer = setTimeout(() => {
+                       this.isActive = false;
+                    }, 2000);
+                });
+        },
+        methods: {
+            sendMessage() {
+                axios.post('/private-messages', { body: this.textMessage, room_id:  this.room.id });
+
+                this.messages.push(this.textMessage);
+
+                this.textMessage = '';
+            },
+            actionUser(){
+                this.channel
+                    .whisper('typing',{
+                        name: this.user.name
+                    })
+            }
+        }
+    }
+</script>
